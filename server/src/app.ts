17ad740438799e5336670cd4fce3474ab5eba02c
ZinHt9tHlaing <phyoearkar267@gmail.com>
2025-08-13 13:49:01 +0630

    Use morgan as request logger

---
+++ server/src/app.ts
@@ -0,0 +1,11 @@
+import express from "express";
+import helmet from "helmet";
+import compression from "compression";
+import cors from "cors";
+import morgan from "morgan";
+
+export const app = express();
+
+app.use(morgan("dev")); // request logger   
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
