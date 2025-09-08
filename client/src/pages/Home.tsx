52464972df88c21ce52a0860cc6fc3bd0a9df5f6
ZinHt9tHlaing <phyoearkar267@gmail.com>
2025-09-08 23:43:30 +0630

    Hero section format

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,7 +1,38 @@
+import { Button } from "@/components/ui/button";
+import Couch from "../data/images/couch.png";
+import { Link } from "react-router-dom";
+
 const Home = () => {
   return (
-    <div className="text-4xl mt-8">Home</div>
-  )
-}
+    <div className="container mx-auto">
+      <div className="flex flex-col items-center lg:flex-row lg:justify-between">
+        {/* Text Section */}
+        <div className="text-center lg:text-left">
+          <h1 className="text-own mb-4 text-4xl font-extrabold lg:mb-8 lg:text-6xl">
+            Modern Interior Design Studio
+          </h1>
+          <p>
+            Furniture is an essential component of any living space, providing
+            functionality, comfort, and aesthetic appeal.
+          </p>
+          <div>
+            <Button asChild className="duration-200 active:scale-90">
+              <Link to="#">Shop Now</Link>
+            </Button>
+            <Button
+              asChild
+              variant="outline"
+              className="duration-200 active:scale-90"
+            >
+              <Link to="#">Explore</Link>
+            </Button>
+          </div>
+        </div>
+        {/* Image Section */}
+        <img src={Couch} alt="Couch" className="" />
+      </div>
+    </div>
+  );
+};
 
-export default Home
+export default Home;
