2aa78ecc54efc4166867e228ba06214f3cd7288f
ZinHt9tHlaing <phyoearkar267@gmail.com>
2025-09-09 20:20:51 +0630

    Custom color in tailwind css

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -7,29 +7,32 @@ const Home = () => {
     <div className="container mx-auto">
       <div className="flex flex-col items-center lg:flex-row lg:justify-between">
         {/* Text Section */}
-        <div className="text-center lg:text-left">
+        <div className="my-8 text-center lg:mt-16 lg:mb-0 lg:w-2/5 lg:text-left">
           <h1 className="text-own mb-4 text-4xl font-extrabold lg:mb-8 lg:text-6xl">
             Modern Interior Design Studio
           </h1>
-          <p>
+          <p className="text-own mb-6 lg:mb-8">
             Furniture is an essential component of any living space, providing
             functionality, comfort, and aesthetic appeal.
           </p>
           <div>
-            <Button asChild className="duration-200 active:scale-90">
+            <Button
+              asChild
+              className="mr-2 rounded-full bg-orange-300 px-8 py-6 text-base font-bold duration-200 active:scale-90"
+            >
               <Link to="#">Shop Now</Link>
             </Button>
             <Button
               asChild
               variant="outline"
-              className="duration-200 active:scale-90"
+              className="text-own rounded-full px-8 py-6 text-base font-bold duration-200 active:scale-90"
             >
               <Link to="#">Explore</Link>
             </Button>
           </div>
         </div>
         {/* Image Section */}
-        <img src={Couch} alt="Couch" className="" />
+        <img src={Couch} alt="Couch" className="w-full lg:w-3/5" />
       </div>
     </div>
   );
