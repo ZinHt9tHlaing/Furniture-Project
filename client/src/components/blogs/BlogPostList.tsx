e814511dfe830519bcaf35f66272a1ee7d1c7bae
ZinHt9tHlaing <phyoearkar267@gmail.com>
2025-09-11 18:41:35 +0630

    create Blog Page

---
+++ client/src/components/blogs/BlogPostList.tsx
@@ -0,0 +1,38 @@
+import { Post } from "@/types";
+import { Link } from "react-router-dom";
+
+interface PostProps {
+  posts: Post[];
+}
+
+const BlogPostList = ({ posts }: PostProps) => {
+  return (
+    <div className="my-8 grid grid-cols-1 gap-16 px-4 md:grid-cols-2 md:px-0 lg:grid-cols-3">
+      {posts.map((post) => (
+        <div className="flex-col">
+          <Link to={`/blogs/${post.id}`} key={post.id}>
+            <img
+              src={post.image}
+              alt={post.title}
+              loading="lazy"
+              decoding="async"
+              className="mb-4 w-full rounded-xl"
+            />
+          </Link>
+          <h2 className="line-clamp-1 text-xl font-extrabold">{post.title}</h2>
+          <h3 className="my-2 line-clamp-3 text-base font-[400]">
+            {post.content}
+          </h3>
+          <div className="text-sm">
+            <span>
+              by<span className="font-[600]"> {post.author} </span>on
+              <span className="font-[600]"> {post.updated_at}</span>
+            </span>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default BlogPostList;
--- client/src/pages/blogs/Blog.tsx
+++ client/src/pages/blogs/Blog.tsx
@@ -1,8 +1,15 @@
+import BlogPostList from "@/components/blogs/BlogPostList";
+import { posts } from '../../data/posts';
 
 const Blog = () => {
   return (
-    <div>Blog</div>
-  )
-}
+    <div className="container mx-auto mt-8">
+      <h1 className="text-center text-2xl font-bold md:text-left">
+        Latest Blog Posts
+      </h1>
+      <BlogPostList posts={posts} />
+    </div>
+  );
+};
 
-export default Blog
+export default Blog;
