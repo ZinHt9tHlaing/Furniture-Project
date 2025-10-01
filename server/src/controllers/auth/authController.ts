34363876bf047c456b0f626d124c7fd408736881
ZinHt9tHlaing <phyoearkar267@gmail.com>
2025-10-01 23:06:53 +0630

    Validation Error Handler

--- server/src/controllers/auth/authController.ts
+++ server/src/controllers/auth/authController.ts
@@ -1,9 +1,40 @@
-import {Request, Response,NextFunction} from "express"
+import { log } from "console";
+import { Request, Response, NextFunction } from "express";
+import { body, validationResult } from "express-validator";
 
-export const registerController = async(req: Request, res: Response, next: NextFunction) => { }
+export const registerController = [
+  body("phone", "Invalid phone number")
+    .trim()
+    .notEmpty()
+    .matches("^[0-9]+$")
+    .isLength({ min: 5, max: 12 }),
+  async (req: Request, res: Response, next: NextFunction) => {
+    const errors = validationResult(req).array({ onlyFirstError: true });
 
-export const verifyOtp = async(req: Request, res: Response, next: NextFunction) => { }
+    if (errors.length > 0) {
+      const error: any = new Error(errors[0].msg);
+      error.status = 400;
+      error.code = "Error_Invalid";
+      return next(error);
+    }
+    res.status(200).json({ message: "register success" });
+  },
+];
 
-export const confirmPassword = async(req: Request, res: Response, next: NextFunction) => { }
+export const verifyOtp = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {};
 
-export const loginController = async(req: Request, res: Response, next: NextFunction) => {}
+export const confirmPassword = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {};
+
+export const loginController = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {};
