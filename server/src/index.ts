20f7a0de608eed059e31996fa6b089dd22766491
ZinHt9tHlaing <phyoearkar267@gmail.com>
2025-10-27 11:12:49 +0630

    setup and connect with redis cloud

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,8 +1,14 @@
 import "dotenv/config";
 import { app } from "./app";
+import { redisClient } from "./config/redisClient";
 
 const PORT = process.env.PORT || 4000;
 
 app.listen(PORT, () => {
   console.log(`Server ready at: http://localhost:${PORT}`);
 });
+
+// Connect to Redis
+redisClient.connect().then(() => {
+  console.log("Connected to Redis");
+}).catch((err) => console.log(err));
